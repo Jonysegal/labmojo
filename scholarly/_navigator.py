9112463589b6c55bb6eb7bde3878f41715d31970
Emile Petrone <emile.petrone@gmail.com>
2020-05-20 15:39:13 -0400

    its aline

--- scholarly/_navigator.py
+++ scholarly/_navigator.py
@@ -17,6 +17,7 @@ from stem.control import Controller
 from fake_useragent import UserAgent
 from .publication import _SearchScholarIterator
 from .author import Author
+from .publication import Publication
 import sys
 
 _GOOGLEID = hashlib.md5(str(random.random()).encode('utf-8')).hexdigest()[:16]
@@ -28,58 +29,67 @@ _HEADERS = {
 _HOST = 'https://scholar.google.com{0}'
 
 _SCHOLARCITERE = r'gs_ocit\(event,\'([\w-]*)\''
-
-_TIMEOUT = 2
-
-_PROXIES = {
-    "http": None,
-    "https": None,
-}
-
-_HTTP_PROXY = None
-_HTTPS_PROXY = None
-
-_TOR_SOCK = None
-_TOR_CONTROL = None
+_PUBSEARCH = '"/scholar?hl=en&q={0}"'
 
 
-if sys.platform.startswith("linux"):
-    _TOR_SOCK = "socks5://127.0.0.1:9050"
-    _TOR_CONTROL = 9051
-elif sys.platform.startswith("win"):
-    _TOR_SOCK = "socks5://127.0.0.1:9150"
-    _TOR_CONTROL = 9151
-else:
-    _TOR_SOCK = "socks5://127.0.0.1:9050"
-    _TOR_CONTROL = 9051
+class Singleton(type):
+    _instances = {}
 
-class Navigator(object):
-    """I did not call it browser because there are other packages with
-    that exact name. -Victor
-    """
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args,
+                                                                 **kwargs)
+        return cls._instances[cls]
+
+
+class Navigator(object, metaclass=Singleton):
+    """A class used to navigate pages on google scholar."""
+
     def __init__(self):
-        # TODO: Implement Singleton pattern since we don't need multiple navs.
         super(Navigator, self).__init__()
         logging.basicConfig(filename='scholar.log', level=logging.INFO)
         self.logger = logging.getLogger('scholarly')
-        self._tor = self._tor_works()
+        self._tor = False
+        self._proxy = False
+        self._setup_tor()
+        self._TIMEOUT = 10
 
-    def _get_page(self, pagerequest: str):
-        """Return the data for a page on scholar.google.com"""
-        self.logger.info("Getting %s", pagerequest)
+    def _setup_tor(self):
+        """Initialized ToR Proxy"""
+
+        # Tor uses the 9050 port as the default socks port
+        # on windows 9150 for socks and 9151 for control
+        if sys.platform.startswith("linux") or sys.platform.startswith("darwin"):
+            self._TOR_SOCK = "socks5://127.0.0.1:9050"
+            self._TOR_CONTROL = 9051
+        elif sys.platform.startswith("win"):
+            self._TOR_SOCK = "socks5://127.0.0.1:9150"
+            self._TOR_CONTROL = 9151
+
+        self.proxies = {'http': self._TOR_SOCK,
+                        'https': self._TOR_SOCK}
+
+        self._tor = self._proxy_works()
+        print("Tor: %s" % self._tor)
+
+    def _get_page(self, pagerequest: str) -> str:
+        """Return the data from a webpage
+
+        :param pagerequest: the page url
+        :type pagerequest: str
+        :returns: the text from a webpage
+        :rtype: {str}
+        :raises: Exception
+        """
+        self.logger.info("%s: Getting %s" % (time.time(), pagerequest))
         resp = None
         while True:
-            # If Tor is running we use the proxy
-            # Did not use with for shorter indented lines -V
+            # Use ToR by default. If proxy was setup, use it.
+            # Otherwise the local IP is used
             session = requests.Session()
-            if self._tor:
+            if self._tor or self._proxy:
 
-                # Tor uses the 9050 port as the default socks port
-                # on windows 9150 for socks and 9151 for control
-                session.proxies = {'http':  _TOR_SOCK,
-                                   'https': _TOR_SOCK}
-            else:
-                session.proxies = _PROXIES
+                session.proxies = self.proxies
 
             try:
                 _HEADERS['User-Agent'] = UserAgent().random
@@ -87,12 +97,7 @@ class Navigator(object):
                 resp = session.get(pagerequest,
                                    headers=_HEADERS,
                                    cookies=_COOKIES,
-                                   timeout=_TIMEOUT)
-                if resp.status_code == 429:
-                    print('Timeout')
-                    print("End " + str(time.time()))
-                    session.close()
-                    import sys; sys.exit()
+                                   timeout=self._TIMEOUT)
 
                 if resp.status_code == 200:
                     if self._has_captcha(resp.text):
@@ -101,7 +106,7 @@ class Navigator(object):
                         session.close()
                         return resp.text
                 else:
-                    self.logger.info(f"""Got a response code {resp.status_code}.
+                    self.logger.info(f"""Response code {resp.status_code}.
                                     Retrying...""")
                     raise Exception(f"Status code {resp.status_code}")
 
@@ -110,29 +115,35 @@ class Navigator(object):
                 self.logger.info(err)
                 # Check if Tor is running and refresh it
                 self.logger.info("Refreshing Tor ID...")
+                session.close()
                 if self._tor:
                     self._refresh_tor_id()
-                session.close()
 
-    def _tor_works(self) -> bool:
-        """ Checks if Tor is working"""
+    def _proxy_works(self) -> bool:
+        """Checks if a proxy is working
+
+        :returns: whether the proxy is working or not
+        :rtype: {bool}
+        """
         with requests.Session() as session:
-            session.proxies = {
-                'http': _TOR_SOCK,
-                'https': _TOR_SOCK
-            }
+            session.proxies = self.proxies
             try:
                 # Changed to twitter so we dont ping google twice every time
                 resp = session.get("http://www.twitter.com")
-                self.logger.info("TOR Works!")
+                self.logger.info("Proxy Works!")
                 return resp.status_code == 200
             except Exception as e:
-                self.logger.info(f"Tor not working: Exception {e}")
+                self.logger.info(f"Proxy not working: Exception {e}")
                 return False
 
     def _refresh_tor_id(self) -> bool:
+        """Refreshes the id by using a new ToR node
+
+        :returns: Whether or not the refresh was succesfull
+        :rtype: {bool}
+        """
         try:
-            with Controller.from_port(port=_TOR_CONTROL) as controller:
+            with Controller.from_port(port=self._TOR_CONTROL) as controller:
                 controller.authenticate(password="scholarly_password")
                 controller.signal(Signal.NEWNYM)
             return True
@@ -141,22 +152,29 @@ class Navigator(object):
             self.logger.info(err)
             return False
 
-    def use_proxy(self, http: str, https: str):
-        """ Routes scholarly through a proxy (e.g. tor).
-            Requires pysocks
-            Proxy must be running."""
-        self.logger.info("Enabling proxies: http=%r https=%r", http, https)
-        _PROXIES = {
-            "http": http,
-            "https": https,
-        }
+    def _use_proxy(self, http: str, https: str):
+        """Allows user to use their own proxy
 
-    def use_tor(self):
-        self.logger.info("Setting tor as the proxy")
-        self._use_proxy(http=_TOR_SOCK,
-                        https=_TOR_SOCK)
+        By using this function the user will be using their own proxy and not
+        ToR. The proxy must be running. ToR will be disabled.
+        :param http: the http proxy
+        :type http: str
+        :param https: the https proxy
+        :type https: str
+        """
+        self.logger.info("Enabling proxies: http=%r https=%r", http, https)
+        self.proxies = {'http': http, 'https': https}
+        self._tor = False
+        self._proxy = self._proxy_works()
 
     def _has_captcha(self, text: str) -> bool:
+        """Tests whether an error or captcha was shown.
+
+        :param text: the webpage text
+        :type text: str
+        :returns: whether or not an error occurred
+        :rtype: {bool}
+        """
         flags = ["Please show you're not a robot",
                  "network may be sending automated queries",
                  "have detected unusual traffic from your computer",
@@ -172,7 +190,7 @@ class Navigator(object):
         res = BeautifulSoup(html, 'html.parser')
         try:
             self.publib = res.find('div', id='gs_res_glb').get('data-sva')
-        except:
+        except Exception:
             pass
         return res
 
@@ -182,13 +200,14 @@ class Navigator(object):
 
         while True:
             rows = soup.find_all('div', 'gsc_1usr')
-            self.logger.info("Found %d authors", len(rows))
+            self.logger.info("%s: Found %d authors" % (time.time(), len(rows)))
             for row in rows:
                 yield Author(self, row)
-            next_button = soup.find(
-                class_='gs_btnPR gs_in_ib gs_btn_half gs_btn_lsb gs_btn_srt gsc_pgn_pnx')
+            cls1 = 'gs_btnPR gs_in_ib gs_btn_half '
+            cls2 = 'gs_btn_lsb gs_btn_srt gsc_pgn_pnx'
+            next_button = soup.find(class_=cls1+cls2)  # Can be improved
             if next_button and 'disabled' not in next_button.attrs:
-                self.logger.info("Loading next page of authors")
+                self.logger.info("%s: Loading next page of authors" % time.time())
                 url = next_button['onclick'][17:-1]
                 url = codecs.getdecoder("unicode_escape")(url)[0]
                 soup = self._get_soup(url)
@@ -196,5 +215,29 @@ class Navigator(object):
                 self.logger.info("No more author pages")
                 break
 
+    def search_publication(self, url: str,
+                           filled: bool = False) -> Publication:
+        """Search by scholar query and return a single Publication object
+
+        :param url: the url to be searched at
+        :type url: str
+        :param filled: Whether publication should be filled, defaults to False
+        :type filled: bool, optional
+        :returns: a publication object
+        :rtype: {Publication}
+        """
+        soup = self._get_soup(url)
+        res = Publication(self, soup.find_all('div', 'gs_or')[0], 'scholar')
+        if filled:
+            res.fill()
+        return res
+
     def search_publications(self, url: str) -> _SearchScholarIterator:
+        """Returns a Publication Generator given a url
+
+        :param url: the url where publications can be found.
+        :type url: str
+        :returns: An iterator of Publications
+        :rtype: {_SearchScholarIterator}
+        """
         return _SearchScholarIterator(self, url)
--- script.py
+++ script.py
@@ -1,13 +1,8 @@
 import json
 import time
 from scholarly import scholarly
-
-from fp.fp import FreeProxy
 from multiprocessing import Pool
 
-proxies = {'http' : 'socks5://127.0.0.1:9050', 'https': 'socks5://127.0.0.1:9050'}
-
-# proxy = FreeProxy(rand=True, timeout=1, country_id=['US', 'CA']).get()
 
 
 def save_file(pi, file_name):
@@ -24,24 +19,51 @@ def get_email_host(pi):
 def get_lastname(pi):
     return pi['name'].split()[-1]
 
+def clean_author(pi):
+    remove_list = ['nav' , '_sections', '_filled']
+    for x in remove_list:
+        if x in pi['scholarly']:
+            del pi['scholarly'][x]
+
+    num_coauthors = len(pi['scholarly']['coauthors'])
+    if num_coauthors:
+        x = 0
+        while x < num_coauthors:
+            pi['scholarly']['coauthors'][x] = pi['scholarly']['coauthors'][x].__dict__
+            for item in remove_list:
+                try:
+                    del pi['scholarly']['coauthors'][x][item]
+                except:
+                    pass
+            x += 1
+
+    num_pubs = len(pi['scholarly']['publications'])
+    if num_pubs:
+        x = 0
+        while x < num_pubs:
+            pi['scholarly']['publications'][x] = pi['scholarly']['publications'][x].__dict__
+            for item in remove_list:
+                try:
+                    del pi['scholarly']['publications'][x][item]
+                except:
+                    pass
+            x += 1
+    return pi
+
 def save_result(pi, result):
     try:
         result.fill()
         pi['scholarly'] = result.__dict__
-        if 'nav' in pi['scholarly']:
-            del pi['scholarly']['nav']
-        if '_sections' in pi['scholarly']:
-            del pi['scholarly']['_sections']
-        if '_filled' in pi['scholarly']:
-            del pi['scholarly']['_filled']
         email_host = get_email_host(pi)
-        print("%s == %s" % (
+        print("*** Success: %s == %s" % (
             pi['name'],
             result.name,
         ))
+        pi = clean_author(pi)
         save_file(pi, 'results/results.json')
     except Exception as e:
-        print("Error: " + str)
+        print("Error: " + str(e))
+        import pdb; pdb.set_trace()
 
 def find_best_match(pi, results):
     email_host = get_email_host(pi)
@@ -65,38 +87,47 @@ def set_scholarly(pi, results, lastname):
     return pi
 
 
+def query_scholarly(pi, query):
+    print('Searching: %s' % query)
+    results = scholarly.search_author(query)
+    lastname = get_lastname(pi)
+    new_pi = set_scholarly(pi, results, lastname)
+
+
 def scholarly_search(pi):
-    if 'email' in pi and pi['email'] != None:
-        names = pi['name'].split()
-        if '.' in names[0]:
-            firstname = names[1]
-        else:
-            firstname = names[0]
-        lastname = get_lastname(pi)
-        scholarly.nav.use_proxy(**proxies)
-        results = scholarly.search_author(pi['name'])
-        new_pi = set_scholarly(pi, results, lastname)
-        if not 'scholarly' in new_pi:
-            results = scholarly.search_author("%s %s" % (firstname, lastname))
-            new_pi = set_scholarly(pi, results, lastname)
-        if not 'scholarly' in new_pi:
-            results = scholarly.search_author("%s %s" % (firstname[0], lastname))
-            new_pi = set_scholarly(pi, results, lastname)
-        if not 'scholarly' in pi and names[0][0] != firstname[0]:
-            results = scholarly.search_author("%s %s" % (names[0][0], lastname))
-            new_pi = set_scholarly(pi, results, lastname)
+    names = pi['name'].split()
+    if '.' in names[0]:
+        firstname = names[1]
+    else:
+        firstname = names[0]
+    lastname = get_lastname(pi)
+    print('Searching: %s' % pi['name'])
+    results = scholarly.search_author(pi['name'])
+    new_pi = set_scholarly(pi, results, lastname)
+
+    # queries = [
+    #     "%s %s" % (firstname, lastname),
+    #      "%s %s" % (firstname[0], lastname),
+    #      "%s %s" % (names[0][0], lastname)
+    # ]
+    #
+    # for x in queries:
+    #     if not 'scholarly' in new_pi:
+    #         new_pi = query_scholarly(pi, x)
+
     if not 'scholarly' in pi:
         save_file(pi, 'results/nomatch.json')
         print("-----------------Not Found----------------------------")
 
 
 if __name__ == "__main__":
-    with open('test.json', 'r+') as file:
+    with open('all.json', 'r+') as file:
         new_data = {'data':[{}]}
         data = json.load(file)
         print("Start " + str(time.time()))
         for pi in data['data']:
-            scholarly_search(pi)
+            if 'email' in pi and pi['email'] != None:
+                scholarly_search(pi)
             # time.sleep(2)
         # with Pool(5) as p:
         #     p.map(scholarly_search, data['data'])
