305c4c70792aeb4f4d759b198115cc03a6081766
Emile Petrone <emile.petrone@gmail.com>
2020-05-21 11:01:36 -0400

    Data - do not f up

--- script.py
+++ script.py
@@ -1,134 +1,235 @@
 import json
 import time
 from scholarly import scholarly
-from multiprocessing import Pool
 
+
+LEADS = 'nomatch-2.json'
+RESULTS = 'results/nomatch-3.json'
+
+
+################################################################################
+# Master File Writing function
+################################################################################
 
 
 def save_file(pi, file_name):
-    m = open(file_name,'a')
-    json.dump(pi, m)
-    m.write(',')
-    m.close()
+    m = open(file_name,'a')  # Amend mode
+    json.dump(pi, m)  # Write JSON
+    m.write(',')  # Add comma
+    m.close()  # Close the file
     return
 
-def get_email_host(pi):
-    at_pos = pi['email'].find('@')
-    return pi['email'][at_pos:]
 
-def get_lastname(pi):
-    return pi['name'].split()[-1]
+################################################################################
+# Pre Save Clean
+################################################################################
 
-def clean_author(pi):
+def remove_scholarly_objects(pi, remove_list, key_name):
+    num_to_remove = len(pi['scholarly'][key_name])
+    if num_to_remove:
+        x = 0
+        while x < num_to_remove:
+            pi['scholarly'][key_name][x] = pi['scholarly'][key_name][x].__dict__
+            for item in remove_list:
+                try:
+                    del pi['scholarly'][key_name][x][item]
+                except:
+                    pass
+            x += 1
+    return pi
+
+def clean_result_before_save(pi):
+    # Master List to remove from everywhere
     remove_list = ['nav' , '_sections', '_filled']
     for x in remove_list:
         if x in pi['scholarly']:
             del pi['scholarly'][x]
 
-    num_coauthors = len(pi['scholarly']['coauthors'])
-    if num_coauthors:
-        x = 0
-        while x < num_coauthors:
-            pi['scholarly']['coauthors'][x] = pi['scholarly']['coauthors'][x].__dict__
-            for item in remove_list:
-                try:
-                    del pi['scholarly']['coauthors'][x][item]
-                except:
-                    pass
-            x += 1
-
-    num_pubs = len(pi['scholarly']['publications'])
-    if num_pubs:
-        x = 0
-        while x < num_pubs:
-            pi['scholarly']['publications'][x] = pi['scholarly']['publications'][x].__dict__
-            for item in remove_list:
-                try:
-                    del pi['scholarly']['publications'][x][item]
-                except:
-                    pass
-            x += 1
+    # Sections that contain Scholarly Objects
+    fields = ['coauthors', 'publications']
+    for field in fields:
+        pi = remove_scholarly_objects(pi, remove_list, field)
     return pi
+
+
+################################################################################
+# Saving Result
+################################################################################
+
 
 def save_result(pi, result):
     try:
         result.fill()
         pi['scholarly'] = result.__dict__
-        email_host = get_email_host(pi)
         print("*** Success: %s == %s" % (
             pi['name'],
             result.name,
         ))
-        pi = clean_author(pi)
+        pi = clean_result_before_save(pi)
         save_file(pi, 'results/results.json')
     except Exception as e:
         print("Error: " + str(e))
         import pdb; pdb.set_trace()
 
-def find_best_match(pi, results):
-    email_host = get_email_host(pi)
+
+################################################################################
+# Finding the best results
+################################################################################
+
+
+def find_best_result(pi, results):
     maybe = []
     for r in results:
         if type(r).__name__ == 'Author':
-            if email_host == r.email:
+            if pi['email_host'] == r.email:
                 maybe.append(r)
     for r in maybe:
-        lastname = get_lastname(pi)
-        if lastname in r.name:
+        # Need to also make sure the name is a match
+        r_firstname = r.name.split()[0]
+        if pi['lastname'] in r.name and r_firstname in pi['name']:
             return r
     return None
 
 
-def set_scholarly(pi, results, lastname):
-    email_host = get_email_host(pi)
-    result = find_best_match(pi, results)
-    if result:
-        save_result(pi, result)
-    return pi
+################################################################################
+# Search Scholarly
+################################################################################
 
 
 def query_scholarly(pi, query):
     print('Searching: %s' % query)
     results = scholarly.search_author(query)
-    lastname = get_lastname(pi)
-    new_pi = set_scholarly(pi, results, lastname)
+    result = find_best_result(pi, results)
+    if result:
+        save_result(pi, result)
+    return pi
+
+
+################################################################################
+# Clean The Name
+################################################################################
+
+
+def remove_curd_at_start(name):
+    remove_list = [
+        'Dr.',
+    ]
+    for x in remove_list:
+        name = name.replace(x, '')
+    return name
+
+def remove_crud_at_end(name):
+    remove_list = [
+        'Ph.D.',
+        'B.S.',
+        'M.D.',
+        'M.S.',
+        ' MB',
+        'BChir',
+        'BSc.',
+        'PhD',
+        'FRCP',
+        'D.V.M.',
+        'Dr.',
+        ', '
+    ]
+    for x in remove_list:
+        name = name.replace(x, '')
+    return name
+
+def get_lastname(pi):
+
+    # Test For Last Name First
+    if ',' in pi['name'].split()[0]:
+        return remove_crud_at_end(pi['name'].split()[0])
+
+    # Else Last name is at the end
+    cleaned_name = scrub_name(pi['name'])
+    return cleaned_name.strip().split()[-1]
+
+
+################################################################################
+# Search Scholarly
+################################################################################
 
 
 def scholarly_search(pi):
-    names = pi['name'].split()
+
+    queries = [
+        pi['name'],  # Search what came from website
+         "%s %s" % (pi['firstname'], pi['lastname']),  # By scrubbed name
+         "%s %s" % (pi['firstname'][0], pi['lastname']),  # search by First Initial
+    ]
+    for x in queries:
+        print('Searching: %s' % x)
+        if not 'scholarly' in pi:
+            pi = query_scholarly(pi, x)
+
+    if not 'scholarly' in pi:
+        save_file(pi, RESULTS)
+        print("-----------------Not Found----------------------------")
+
+
+################################################################################
+# Setup the Data to go through search
+################################################################################
+
+
+def has_two_names(pi):
+    try:
+        # Catch weird name stuff
+        name = remove_curd_at_start(pi['name'])
+        pi['cleaned_name'] = name
+        names = name.split()
+    except:
+        names = 0
+
+    if len(names) > 1:
+        return True
+    return False
+
+def set_first_name(pi):
     if '.' in names[0]:
         firstname = names[1]
     else:
         firstname = names[0]
-    lastname = get_lastname(pi)
-    print('Searching: %s' % pi['name'])
-    results = scholarly.search_author(pi['name'])
-    new_pi = set_scholarly(pi, results, lastname)
+    pi['firstname'] = firstname
+    return pi
 
-    # queries = [
-    #     "%s %s" % (firstname, lastname),
-    #      "%s %s" % (firstname[0], lastname),
-    #      "%s %s" % (names[0][0], lastname)
-    # ]
-    #
-    # for x in queries:
-    #     if not 'scholarly' in new_pi:
-    #         new_pi = query_scholarly(pi, x)
+def set_last_name(pi):
+    pi['lastname'] = get_lastname(pi)
+    return pi
 
-    if not 'scholarly' in pi:
-        save_file(pi, 'results/nomatch.json')
-        print("-----------------Not Found----------------------------")
+def set_email_host(pi):
+    at_pos = pi['email'].find('@')
+    pi['email_host'] = pi['email'][at_pos:]
+    return pi
+
+def set_initial_data(pi):
+    pi = set_email_host(pi)
+    pi = set_first_name(pi)
+    pi = set_last_name(pi)
+    return pi
+
+
+################################################################################
+# Main
+################################################################################
 
 
 if __name__ == "__main__":
-    with open('all.json', 'r+') as file:
+    with open(LEADS, 'r+') as file:
         new_data = {'data':[{}]}
         data = json.load(file)
         print("Start " + str(time.time()))
         for pi in data['data']:
+            # Make sure there is an Email
             if 'email' in pi and pi['email'] != None:
-                scholarly_search(pi)
-            # time.sleep(2)
-        # with Pool(5) as p:
-        #     p.map(scholarly_search, data['data'])
+                # Set some initial data before search
+                pi = set_initial_data(pi)
+                # Check before searching
+                if has_two_names(pi):
+                    # Search Scholarly
+                    scholarly_search(pi)
+
         print("End " + str(time.time()))
--- test.py
+++
@@ -1,7 +0,0 @@
-from scholarly import scholarly
-# default values are shown below
-proxies = {'http' : 'socks5://127.0.0.1:9050', 'https': 'socks5://127.0.0.1:9050'}
-scholarly.nav.use_proxy(**proxies)
-# If proxy is correctly set up, the following runs through it
-author = next(scholarly.search_author('Steven A Cholewiak'))
-print(author)
